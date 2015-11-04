dae9459c3ee9a3808401abd38ca547f3364466d4
curlychoi <curlychoi@gmail.com>
2015-11-04 13:18:18 +0900

    배추모바일 경로 설정

--- _common.php
+++ _common.php
@@ -1,13 +1,25 @@
 <?php
-$g4_path = "../../.."; // common.php 의 상대 경로
-if (is_file("$g4_path/common.php"))
-    include_once("$g4_path/common.php");
+// common.php 의 상대 경로
+$g4_path = "../../..";
+if (is_file($g4_path."/common.php")) {
+    // pc 스킨
+    include_once($g4_path."/common.php");
+}
 else {
     $g4_path = "../../../..";
-    include_once("$g4_path/common.php");
+    if (is_file($g4_path."/common.php")) {
+        // g5 모바일 스킨
+        include_once($g4_path."/common.php");
+    }
+    else {
+        $g4_path = "../../../../..";
+        // 배추 모바일 스킨
+        include_once($g4_path."/common.php");
+    }
 }
 
 if (defined('G5_PATH'))
     header("Content-Type: text/html; charset=utf-8");
 else
-    header("Content-Type: text/html; charset=$g4[charset]");
+    header("Content-Type: text/html; charset=".$g4['charset']);
+
--- mw.geditor/_common.php
+++ mw.geditor/_common.php
@@ -1,8 +1,19 @@
 <?php
-$g4_path = "../../../.."; // common.php 의 상대 경로
-if (is_file("$g4_path/common.php"))
-    include_once("$g4_path/common.php");
+// common.php 의 상대 경로
+$g4_path = "../../../..";
+if (is_file($g4_path."/common.php")) {
+    // pc 스킨
+    include_once($g4_path."/common.php");
+}
 else {
     $g4_path = "../../../../..";
-    include_once("$g4_path/common.php");
+    if (is_file($g4_path."/common.php")) {
+        // g5 모바일 스킨
+        include_once($g4_path."/common.php");
+    }
+    else {
+        $g4_path = "../../../../../..";
+        // 배추 모바일 스킨
+        include_once($g4_path."/common.php");
+    }
 }
