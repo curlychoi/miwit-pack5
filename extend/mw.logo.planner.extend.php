923809f047e97271ef08324dbc20f03b5bc9cce3
curlychoi <curlychoi@gmail.com>
2015-11-18 22:42:15 +0900

    mysqli 지원 (상속자님 제안)

--- extend/mw.logo.planner.extend.php
+++ extend/mw.logo.planner.extend.php
@@ -1,6 +1,8 @@
-<?
+<?php
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+if (defined('G5_PATH') && @is_file(G5_EXTEND_PATH."/mw.g5.adapter.extend.php"))
+    include_once(G5_EXTEND_PATH."/mw.g5.adapter.extend.php");
 
 include_once("$g4[path]/plugin/logo-planner/_config.php");
 include_once("$g4[path]/plugin/logo-planner/_lib.php");
-?>
