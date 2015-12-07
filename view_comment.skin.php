2f26112cc3b42aaf1bf6126e1542ca0e6ba490c0
curlychoi <curlychoi@gmail.com>
2015-12-07 15:50:58 +0900

    댓글 로그, 신고 버튼 글자 출력 반응형 수정

--- view_comment.skin.php
+++ view_comment.skin.php
@@ -542,11 +542,21 @@ for ($i=0; $i<$to_record; $i++) {
         <?php if ($list[$i]['mb_id'] != '@lucky-writing') { ?>
         <div class="comment_buttons">
             <?php if ($history_href) {?>
-            <span class="button"><a href="<?php echo $history_href?>" title="변경기록"><i class='fa fa-history'></i> 로그</a></span>
+            <span class="button">
+                <a href="<?php echo $history_href?>" title="변경기록">
+                    <i class='fa fa-history'></i>
+                    <span class='media-comment-button'>로그</span>
+                </a>
+            </span>
             <?php } ?>
 
             <?php if ($is_member and $list[$i]['singo_href']) { ?>
-            <span class="button"><a href="<?=$list[$i][singo_href]?>"><i class="fa fa-warning"></i> 신고</a></span>
+            <span class="button">
+                <a href="<?=$list[$i][singo_href]?>">
+                    <i class="fa fa-warning"></i>
+                    <span class='media-comment-button'>신고</span>
+                </a>
+            </span>
             <?php } ?>
 
             <span class="mw_basic_comment_url button" value="<?=$list[$i][wr_id]?>">
