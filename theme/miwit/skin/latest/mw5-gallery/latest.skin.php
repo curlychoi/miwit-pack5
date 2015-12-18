ce10834a15ceffd95ed233d344c2554b1108412e
curlychoi <curlychoi@gmail.com>
2015-12-18 12:58:45 +0900

    갤러리 최근게시물 비밀글 처리 (l2zeo님 제안)

--- theme/miwit/skin/latest/mw5-gallery/latest.skin.php
+++ theme/miwit/skin/latest/mw5-gallery/latest.skin.php
@@ -1,6 +1,8 @@
 <?php
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
+global $member;
+
 $css_name = "gallery-{$bo_table}-{$rows}-{$subject_len}";
 
 $css = "{$latest_skin_url}/style.php?bo_table={$bo_table}&rows={$rows}&subject_len={$subject_len}";
@@ -10,12 +12,6 @@ if (function_exists("mw_seo_url"))
     $bo_url = mw_seo_url($bo_table);
 else 
     $bo_url = G5_BBS_URL."/board.php?bo_table=".$bo_table;
-
-// $thumb = get_list_thumbnail($board['bo_table'], $list[$i]['wr_id'], $board['bo_gallery_width'], $board['bo_gallery_height']);
-// mw_get_thumb_path
-// function mw_get_thumb_path($bo_table, $wr_id, $file=null, $thumb_number=null)
-
-global $g4;
 ?>
 <div class="<?php echo $css_name?>">
     <h2><a href="<?php echo $bo_url?>"><?php echo $bo_subject?></a></h2>
@@ -27,6 +23,11 @@ global $g4;
         $thumb = mw_get_thumb_path($bo_table, $list[$i]['wr_id']);
         $img = '<img src="'.$thumb.'">';
 
+        $list[$i] = mw_get_list($list[$i]);
+
+        if (!$thumb)
+            $img = '<div class="noimage"></div>';
+
         if ($member['mb_id']) {
             $list[$i]['subject'] = str_replace("{닉네임}", $member['mb_nick'], $list[$i]['subject']);
             $list[$i]['subject'] = str_replace("{별명}", $member['mb_nick'], $list[$i]['subject']);
@@ -37,11 +38,14 @@ global $g4;
         }
 
         $class = '';
-        if ($list[$i]['icon_secret'])
+        if ($list[$i]['icon_secret'] or $list[$i]['wr_singo_lock'] or $list[$i]['wr_view_lock']) {
             $class.= " secret";
+            $img = '<div class="noimage"><i class="fa fa-lock"></i></div>';
+        }
 
-        if ($list[$i]['icon_new'])
+        if ($list[$i]['icon_new']) {
             $class.= " new";
+        }
 
         if (function_exists("mw_seo_url"))
             $href = mw_seo_url($bo_table, $list[$i]['wr_id']);
@@ -59,6 +63,7 @@ global $g4;
     }
     echo "</ul>";
     ?>
+    <div class="clear"></div>
 </div><!--<?php echo $css_name?>-->
-<div style="font-size:0; line-height:0; clear:both; height:10px;"></div>
+<div class="clear"></div>
 
