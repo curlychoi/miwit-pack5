f9949d8bcd0d0b29e5bdf47d04a63cfa1309ceed
curlychoi <curlychoi@gmail.com>
2016-02-15 10:58:22 +0900

    다운로드 팝업시 그누보드5 로그아웃 버튼 나오는 문제 해결 (도장관님 제안)

--- mw.proc/download.popup.skin.php
+++ mw.proc/download.popup.skin.php
@@ -22,17 +22,48 @@
 include_once("_common.php");
 include_once("$board_skin_path/mw.lib/mw.skin.basic.lib.php");
 
-//if (!$is_member) alert_close("회원만 이용 가능합니다.");
-
-include_once("$g4[path]/head.sub.php");
+$gmnow = gmdate("D, d M Y H:i:s") . " GMT";
+header("Expires: 0"); // rfc2616 - Section 14.21
+header("Last-Modified: " . $gmnow);
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Cache-Control: pre-check=0, post-check=0, max-age=0"); // HTTP/1.1
+header("Pragma: no-cache"); // HTTP/1.0
 ?>
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="<?=$g4['charset']?>">
+<title><?=$g4['title']?></title>
+<script>
+// 자바스크립트에서 사용하는 전역변수 선언
+var g4_path      = "<?=$g4['path']?>";
+var g4_bbs       = "<?=$g4['bbs']?>";
+var g4_bbs_img   = "<?=$g4['bbs_img']?>";
+var g4_url       = "<?=$g4['url']?>";
+var g4_is_member = "<?=$is_member?>";
+var g4_is_admin  = "<?=$is_admin?>";
+var g4_bo_table  = "<?=isset($bo_table)?$bo_table:'';?>";
+var g4_sca       = "<?=isset($sca)?$sca:'';?>";
+var g4_charset   = "<?=$g4['charset']?>";
+var g4_cookie_domain = "<?=$g4['cookie_domain']?>";
+var g4_is_gecko  = navigator.userAgent.toLowerCase().indexOf("gecko") != -1;
+var g4_is_ie     = navigator.userAgent.toLowerCase().indexOf("msie") != -1;
+<? if ($is_admin) { echo "var g4_admin = '{$g4['admin']}';"; } ?>
+</script>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<link href="//code.jquery.com/ui/1.11.2/themes/humanity/jquery-ui.css" rel="stylesheet" />
 <style type="text/css">
 .ci_title { font-size:15px; font-weight:bold; background-color:#efefef; padding:10px; }
 .ci_info { margin:20px 0 0 20px; line-height:20px; font-size:13px; }
 .ci_file { border-top:1px solid #9a9a9a; border-left:1px solid #9a9a9a; border-right:1px solid #d8d8d8; border-bottom:1px solid #d8d8d8; height:20px; font-size:12px; padding:2px; }
 .ci_button { background-color:#efefef; cursor:pointer; }
 .ci_buttons { text-align:center; margin:20px 0 0 0; height:50px; }
+a { color:#000; }
 </style>
+</head>
+<body topmargin="0" leftmargin="0" <?=isset($g4['body_script']) ? $g4['body_script'] : "";?>>
+<a name="g4_head"></a>
 
 <div class="ci_title"> ▶ 파일다운로드 </div>
 
@@ -46,6 +77,9 @@ include_once("$g4[path]/head.sub.php");
 
 </div>
 
-<?
-include_once("$g4[path]/tail.sub.php");
-?>
+<!-- 새창 대신 사용하는 iframe -->
+<iframe width=0 height=0 name='hiddenframe' style='display:none;'></iframe>
+
+</body>
+</html>
+
